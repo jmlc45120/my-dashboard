663a2a6eae11a3585d197fec0ef9b4a0f1fb67c7
Jean-Michel LE COUVIOUR <jeanmilc45@gmail.com>
2025-02-27 14:07:11 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// Déclaration d'une variable réactive `count`
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="count++">Clique moi</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  padding: 10px;
+  font-size: 16px;
+  background-color: #42b883;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/MonPremierComposant.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const message = "Bienvenue dans mon premier composant Vue ! 🎉";
+</script>
+
+<template>
+    <div class="mon-composant">
+        <h2>{{ message }}</h2>
+    </div>
+</template>
+
+<style scoped>
+.mon-composant {
+    padding: 20px;
+    background-color: #f3f4f6;
+    border-radius: 8px;
+    text-align: center;
+    font-size: 1.2rem;
+}
+</style>
